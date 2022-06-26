647f3159b0a72519a0a51c5299cbbfe114b48624
Harsh Kothari <hmkothar@ualberta.ca>
2022-06-26 16:28:09 -0600

    finished basic declarations and implementations

---
+++ graphbuilder.cpp
@@ -0,0 +1,84 @@
+#include "graphdesign.h"
+#include <iostream>
+using namespace std;
+#include <queue>
+#include <string>
+
+int Directedgraph::numNeighbours(string vertex){
+    return neighbours[vertex].size();
+}
+
+int Directedgraph::size(){
+    // size of the graph is the number of nodes; easiest way to find it is by seeing how many items in unordered_map
+    return neighbours.size();
+}
+
+vector<string> Directedgraph::getAllVertices(){
+    vector<string> allVertices;
+    for (pair<string, unordered_set<string>> element: neighbours){
+        allVertices.push_back(element.first);
+    }
+    return allVertices;
+}
+
+void Directedgraph::adding_Vertex(string vertex){
+    if (neighbours.find(vertex)!=neighbours.end()){
+        cout<<"Vertex already exists" << endl;
+    }
+    else{
+        neighbours[vertex];
+    }
+}
+
+ void Directedgraph::adding_Edge(string firstvertex, string secondvertex){
+    neighbours[firstvertex].insert(secondvertex);
+    // adds secondvertex in the set
+ }
+
+ bool Directedgraph::isItAVertex(string vertex){
+    if (neighbours.find(vertex)!=neighbours.end()){
+        return true;
+    }
+    else{
+        return false; 
+    }
+ }
+
+bool Directedgraph::isItAnEdge(string firstvertex, string secondvertex){
+    if (isItAVertex(firstvertex) && isItAVertex(secondvertex)){
+        if (neighbours[firstvertex].find(secondvertex) != neighbours[firstvertex].end()){
+        return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else {
+        cout<<"One of the vertices does not exist in the graph"<< endl;
+        return false; 
+    }
+}
+
+bool Directedgraph::isPathValid(vector<string> path){
+    // path is a walk with no repeated vertices. 
+    bool pathway=false;
+    for (int i=0; i<path.size()-1; i++){
+        if(isItAVertex(path[i]) && isItAnEdge(path[i],path[i+1])){
+            // if it is a neighbour 
+            pathway=true;
+        }
+        else{
+            pathway=false;
+        }
+    }
+    return pathway;
+}
+
+
+
+
+
+
+
+
+
---
+++ graphdesign.h
@@ -0,0 +1,60 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+/*
+  Represents a graph using an adjacency list representation.
+  Vertices will be courses' names;
+*/
+class Directedgraph {
+public:
+
+  // return the number of outgoing neighbours of v
+  int numNeighbours(string vertex);
+
+  // returns the number of nodes
+  int size();
+
+  // return a vector with all vertices
+  vector<string> getAllVertices();
+  // add a vertex, does nothing if it already exists
+  void adding_Vertex(string vertex);
+
+  // adds an edge, creating the vertices if they do not exist
+  // if the edge already existed, does nothing
+  void adding_Edge(string firstvertex, string secondvertex);
+
+  // returns true if and only if v is a vertex in the graph
+  bool isItAVertex(string vertex);
+
+
+  // returns true if and only if (u,v) is an edge in the graph
+  // will certainly return false if neither vertex is in the graph
+  bool isItAnEdge(string firstvertex, string secondvertex);
+
+  // returns a const iterator to the neighbours of v
+  unordered_set<int>::const_iterator neighbours(string vertex) const;
+
+  // returns a const iterator to the end of v's neighour set
+  unordered_set<int>::const_iterator endIterator(string vertex) const;
+  
+
+  // returns true if 'path' represents a path on this graph
+  // a path is a walk with no repeated vertices
+  bool isPathValid(vector<string> path);
+
+  unordered_map<string,string> shortestPath(string vertex1, string vertex2);
+
+private:
+  // with this representation the amortized time for 
+  // finding or adding a vertex or an edge is O(1)
+  unordered_map<string, unordered_set<string>> neighbours;
+};
+
+#endif
