986c692a3d0679d440baf483c9b6c1102dab621e
Harsh Kothari <hmkothar@ualberta.ca>
2022-06-29 22:58:26 -0600

    Minor changes: took off namespace std

--- graphBuilder.cpp
+++ graphBuilder.cpp
@@ -1,10 +1,8 @@
 #include "graphDesign.h"
 #include <iostream>
-using namespace std;
-#include <queue>
 #include <string>
 
-int Directedgraph::numNeighbours(string vertex){
+int Directedgraph::numNeighbours(std::string vertex){
     return neighbours[vertex].size();
 }
 
@@ -13,61 +11,57 @@ int Directedgraph::size(){
     return neighbours.size();
 }
 
-vector<string> Directedgraph::getAllVertices(){
-    vector<string> allVertices;
-    for (pair<string, unordered_set<string>> element: neighbours){
+std::vector<std::string> Directedgraph::getAllVertices(){
+    std::vector<std::string> allVertices;
+    for (std::pair<std::string, std::unordered_set<std::string>> element: neighbours){
         allVertices.push_back(element.first);
     }
     return allVertices;
 }
 
-void Directedgraph::adding_Vertex(string vertex){
+void Directedgraph::adding_Vertex(std::string vertex){
     if (neighbours.find(vertex)!=neighbours.end()){
-        cout<<"Vertex already exists" << endl;
-    }
-    else{
+        std::cout<<"Vertex already exists" << std::endl;
+    } else{
         neighbours[vertex];
     }
 }
 
- void Directedgraph::adding_Edge(string firstvertex, string secondvertex){
+ void Directedgraph::adding_Edge(std::string firstvertex, std::string secondvertex){
     neighbours[firstvertex].insert(secondvertex);
     // adds secondvertex in the set
  }
 
- bool Directedgraph::isItAVertex(string vertex){
+ bool Directedgraph::isItAVertex(std::string vertex){
     if (neighbours.find(vertex)!=neighbours.end()){
         return true;
-    }
-    else{
+    } else{
         return false; 
     }
  }
 
-bool Directedgraph::isItAnEdge(string firstvertex, string secondvertex){
+bool Directedgraph::isItAnEdge(std::string firstvertex, std::string secondvertex){
     if (isItAVertex(firstvertex) && isItAVertex(secondvertex)){
         if (neighbours[firstvertex].find(secondvertex) != neighbours[firstvertex].end()){
         return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
     else {
-        cout<<"One of the vertices does not exist in the graph"<< endl;
+        std::cout<<"One of the vertices does not exist in the graph"<< std::endl;
         return false; 
     }
 }
 
-bool Directedgraph::isPathValid(vector<string> path){
+bool Directedgraph::isPathValid(std::vector<std::string> path){
     // path is a walk with no repeated vertices. 
     bool pathway=false;
     for (int i=0; i<path.size()-1; i++){
         if(isItAVertex(path[i]) && isItAnEdge(path[i],path[i+1])){
             // if it is a neighbour 
             pathway=true;
-        }
-        else{
+        } else{
             pathway=false;
         }
     }
--- graphDesign.h
+++ graphDesign.h
@@ -7,8 +7,6 @@
 #include <set>
 #include <string>
 
-using namespace std;
-
 /*
   Represents a graph using an adjacency list representation.
   Vertices will be courses' names;
@@ -17,38 +15,38 @@ class Directedgraph {
 public:
 
   // return the number of outgoing neighbours of v
-  int numNeighbours(string vertex);
+  int numNeighbours(std::string vertex);
 
   // returns the number of nodes
   int size();
 
   // return a vector with all vertices
-  vector<string> getAllVertices();
+  std::vector<std::string> getAllVertices();
   // add a vertex, does nothing if it already exists
-  void adding_Vertex(string vertex);
+  void adding_Vertex(std::string vertex);
 
   // adds an edge, creating the vertices if they do not exist
   // if the edge already existed, does nothing
-  void adding_Edge(string firstvertex, string secondvertex);
+  void adding_Edge(std::string firstvertex, std::string secondvertex);
 
   // returns true if and only if v is a vertex in the graph
-  bool isItAVertex(string vertex);
+  bool isItAVertex(std::string vertex);
 
 
   // returns true if and only if (u,v) is an edge in the graph
   // will certainly return false if neither vertex is in the graph
-  bool isItAnEdge(string firstvertex, string secondvertex);
+  bool isItAnEdge(std::string firstvertex, std::string secondvertex);
 
   // returns true if 'path' represents a path on this graph
   // a path is a walk with no repeated vertices
-  bool isPathValid(vector<string> path);
+  bool isPathValid(std::vector<std::string> path);
 
-  unordered_map<string,string> shortestPath(string vertex1, string vertex2);
+  std::unordered_map<std::string,std::string> shortestPath(std::string vertex1, std::string vertex2);
 
 private:
   // with this representation the amortized time for 
   // finding or adding a vertex or an edge is O(1)
-  unordered_map<string, unordered_set<string>> neighbours;
+  std::unordered_map<std::string, std::unordered_set<std::string>> neighbours;
 };
 
 #endif
